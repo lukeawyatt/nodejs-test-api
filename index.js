58600ea78c5252e898128c56bc9b779e8ade47ff
Luke Wyatt <lwyatt@qhco.com>
2020-04-14 14:46:03 -0400

    Heroku changes 01

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 3000
+
 
 server.use(middlewares)
 server.use(router)
-server.listen(80, () => {
-  console.log('JSON Server is running')
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on ${ PORT }`)
 })
