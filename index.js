a90759a8e16bbdcc455caee2e826d1169346a614
Luke Wyatt <lwyatt@qhco.com>
2020-04-21 10:58:24 -0400

    Session 02 changes, JWT

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const PORT = process.env.PORT || 3000
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
 
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
 
-server.use(middlewares)
-server.use(router)
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on ${ PORT }`)
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 3000;
+
+const rules = auth.rewriter({
+	// Permission rules
+	// posts: 600,
+	// comments: 600,
+	// albums: 600,
+	// photos: 600,
+	// users: 600,
+	// todos: 640
+	secureMessages: 600
 })
+
+server.use(middlewares);
+server.use(auth);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on ${ PORT }`);
+});
